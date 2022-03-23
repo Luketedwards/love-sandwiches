2b2c4bcfafb00eafb7ba4cf6d291cedffc8f6a50
Luketedwards <Luketedwards96@hotmail.com>
2022-03-23 12:26:54 +0000

    Compare sales and stock data

--- run.py
+++ run.py
@@ -65,24 +65,37 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+
 def calculate_surplus_data(sales_row):
     """
-    Uses sale data and stock to calculate excess sandwiches.
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
-    print("calculating surplus data ... \n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
-    pprint(stock)
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
 
 def main():
     """
-    Calls all functions to run program
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
-print("Welcome to Love Sandwiches data automation")
-main()    
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
